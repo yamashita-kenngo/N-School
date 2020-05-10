1851671d6495733132d74ebfbb53cd1618b53eb9
yamashita <yamashita-kenngo@outlook.jp>
2020-05-10 23:36:20 +0900

    再帰関数の中身を実装

--- scala-study/Factorization.scala
+++ scala-study/Factorization.scala
@@ -13,7 +13,14 @@ object Factorization extends App {
     val maxDivisor = sqrt(target).toInt
 
     def factorizationRec(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
-        Map(2 -> 3, 3 -> 1)
+        if (divisor > maxDivisor) {
+            if (num == 1) acc else acc + (num -> 1)
+        } else if (num % divisor == 0) {
+            val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
+            factorizationRec(num / divisor, divisor, nextAcc)
+        } else {
+            factorizationRec(num, divisor + 1, acc)
+        }
     }
     println(factorizationRec(target, 2, Map()))
 }
